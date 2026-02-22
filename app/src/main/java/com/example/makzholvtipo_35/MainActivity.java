ec6e3d55353f4d3e932c5bfe2f03550902843bb2
Akzhol Bratan Vash <manatoffin2006@gmail.com>
2026-02-22 19:25:01 +0500

    SRO-2 Main

--- app/src/main/java/com/example/makzholvtipo_35/MainActivity.java
+++ app/src/main/java/com/example/makzholvtipo_35/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.makzholvtipo_35;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,24 +12,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnActTwo;
+    final String TAG =
+            "States";
 
-    // UI-элементы
-    private TextView Result;        // Текст для вывода результата/подсказок
-    private EditText InputNumbers;  // Поле ввода числа пользователем
-    private Button GRNButton;       // Кнопка проверки числа
-    private Button Reset;           // Кнопка сброса игры
-
-    private int randomNumber;       // Загаданное случайное число
-    private boolean isGameOver = false; // Флаг окончания игры
-    private Random random = new Random(); // Генератор случайных чисел
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,
+                "MainActivity: onCreate()");
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -43,67 +35,50 @@ public class MainActivity extends AppCompatActivity {
             );
             return insets;
         });
-
-        // Привязываем элементы интерфейса по ID из layout
-        Result = findViewById(R.id.Result);
-        InputNumbers = findViewById(R.id.InputNumber);
-        GRNButton = findViewById(R.id.GRNButton);
-        Reset = findViewById(R.id.Reset);
-
-        // Запускаем новую игру при старте Activity
-        resetGame();
-
-        // Обработчик нажатия кнопки "Угадать"
-        GRNButton.setOnClickListener(view -> {
-
-            // Получаем введённое пользователем значение как строку
-            String input = InputNumbers.getText().toString();
-
-            // Если игра уже закончена начинаем новую
-            if (isGameOver) {
-                resetGame();
-                InputNumbers.setText("");
-                Toast.makeText(this, "Игра сброшена", Toast.LENGTH_SHORT).show();
-            }
-
-            // Проверка: если поле пустое просим ввести число
-            if (input.isEmpty()) {
-                Result.setText("Введите число!");
-                return;
-            }
-
-            // Преобразуем строку в число
-            int userGuess = Integer.parseInt(input);
-
-            // Сравниваем с загаданным числом
-            if (userGuess > randomNumber) {
-                Result.setText("Меньше!");
-            } else if (userGuess < randomNumber) {
-                Result.setText("Больше!");
-            } else {
-                // Пользователь угадал
-                Result.setText("Поздравляю! Вы угадали!");
-                isGameOver = true;
-            }
-        });
-
-        // Обработчик кнопки сброса
-        Reset.setOnClickListener(view -> {
-            resetGame();               // Генерируем новое число
-            InputNumbers.setText("");  // Очищаем поле ввода
-            Toast.makeText(this, "Игра сброшена", Toast.LENGTH_SHORT).show(); //Всплывающее окошко
-        });
+        btnActTwo = findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
     }
 
-    // Функция сброса игры и генерации нового числа
-    private void resetGame() {
-        // Генерируем случайное число от 1 до 100 включительно
-        this.randomNumber = random.nextInt(100) + 1;
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnActTwo) {
+            Intent intent = new Intent(this, activity2.class);
+            startActivity(intent);
+        }
+    }
 
-        // Выводим стартовое сообщение
-        Result.setText("Угадайте число от 1 до 100");
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "MainActivity.onStart");
+    }
 
-        // Сбрасываем флаг окончания игры
-        isGameOver = false;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "MainActivity.onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "MainActivity.onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "MainActivity.onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "MainActivity.onDestroy");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "MainActivity.onRestart");
     }
 }
